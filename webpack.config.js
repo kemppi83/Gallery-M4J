372649f3bb60faf4fb76e38fb497f7f33c6ce6ca
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-22 16:01:02 +0100

    linter and jest setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin')
-const webpack = require('webpack')
+  CleanWebpackPlugin,
+} = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
@@ -55,4 +55,4 @@ module.exports = {
       },
     ],
   },
-}
+};
